8821e5d1342e750747f951723abcf3f69a434f57
Aditya Kumar Singh <aditya.trackstar@gmail.com>
2024-11-14 19:53:12 +0530

    added jest and rtl

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 import '../src/index.css'
-import './src/styles/variables.css';
+import '../src/styles/variables.css';
 
 const preview: Preview = {
   parameters: {
